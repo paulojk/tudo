ac484b8e0127da4cffefd08cf1dfa4ffdb3353d7
paulojk <40920215+paulojk@users.noreply.github.com>
2023-06-26 11:30:52 -0500

    Add files via upload

---
+++ .ccaviso.php
@@ -0,0 +1,3 @@
+<?php
+$us='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.51';
+$cookie='PHPSESSID=t953kpnrd8hmou6cthkbm9ekr7';
